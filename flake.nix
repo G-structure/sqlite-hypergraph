2a1a217737726ec7b7e72ba5cac392b44cbaeb9d
luc <luc@forgotten.name>
2024-11-19 14:37:56 -0500

    .

--- flake.nix
+++ flake.nix
@@ -20,12 +20,10 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             emscripten
-            cmake
             gcc
             gnumake
             python3
@@ -33,11 +31,8 @@
           ];
 
           shellHook = ''
-            # Set up build environment
             export SQLITE_VEC_SRC=${sqlite-vec}
             export SQLITE_LEMBED_SRC=${sqlite-lembed}
-
-            # Configure emscripten
             source ${pkgs.emscripten}/share/emscripten/env
           '';
         };
@@ -46,11 +41,10 @@
           pname = "hypergraph-wasm";
           version = "0.1.0";
 
-          src = ./.;
+          src = ./src/hypergraph_db/wasm;
 
           nativeBuildInputs = with pkgs; [
             emscripten
-            cmake
             gcc
             gnumake
             python3
@@ -58,28 +52,15 @@
           ];
 
           buildPhase = ''
-            # Create build directories
+            # Set environment variables
+            export SQLITE_VEC_SRC=${sqlite-vec}
+            export SQLITE_LEMBED_SRC=${sqlite-lembed}
+
+            # Create output directories
             mkdir -p dist/.wasm
 
-            # Build sqlite-vec WASM
-            cp -r $SQLITE_VEC_SRC/* .
-            emmake make wasm
-
-            # Build sqlite-lembed WASM
-            cp -r $SQLITE_LEMBED_SRC/* .
-            emmake make wasm
-
-            # Build our hypergraph WASM module
-            emcc -O3 \
-              -I. \
-              -I$SQLITE_VEC_SRC \
-              -I$SQLITE_LEMBED_SRC \
-              -s WASM=1 \
-              -s EXPORTED_FUNCTIONS='["_malloc", "_free", "_init_hypergraph_db", "_insert_node"]' \
-              -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "stringToUTF8", "UTF8ToString"]' \
-              -s ALLOW_MEMORY_GROWTH=1 \
-              -o dist/.wasm/hypergraph.js \
-              src/hypergraph_db/wasm/hypergraph.c
+            # Build using our Makefile
+            make wasm
           '';
 
           installPhase = ''
